22463d575b0cd55eef74bfbcca3d029a0a187510
hpanahi <hpanahi@uh.edu>
2014-11-05 18:30:31 +0000

    create idea, add idea controller

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,23 @@
+class IdeasController < ApplicationController
+  def show 
+  end
+  def new
+    @idea = Idea.new
+  end
+   def create
+       @idea = Idea.new(idea_params)
+      respond_to do |format|
+       if @idea.save
+         format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+         format.json { render :show, status: :created, location: @idea }
+      else
+        format.html { render :new }
+         format.json { render json: @idea.errors, status: :unprocessable_entity }
+       end
+      end
+  end
+  private
+   def idea_params
+     params.require(:idea).permit(:title,:description)
+   end
+end
