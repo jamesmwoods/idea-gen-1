cc15cd6b7e3e7e8ae21b93713088924fb04edc85
hpanahi <hpanahi@uh.edu>
2014-11-05 18:09:19 +0000

    add idea, add idea associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105173936) do
+ActiveRecord::Schema.define(version: 20141105175857) do
+
+  create_table "ideas", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
